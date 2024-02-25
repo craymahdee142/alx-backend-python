962d45220a3973ce2b00e3ec1cb8d13fbe07e150
root <root@DESKTOP-H9275MT.localdomain>
2024-02-25 21:56:31 +0000

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """Coroutine that generates random numbers between 0 and 10, one number
     at a time.
